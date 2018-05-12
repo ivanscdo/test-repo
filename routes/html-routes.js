9a7e264f28b4fddbf7c22726870c304828dbfac1
Ivan Saucedo <ivan.scdo@gmail.com>
2018-05-12 00:06:15 -0500

    sa512.00:05- completed:views-routes-js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,19 @@
 var express = require("express"),
-    router  = express.Router();
+    router  = express.Router(),
+    db      = require("../models");
 
 router.get("/", function(req, res) {
     // res.send("red wine, success!");
-    
-})
+    db.Test.findAll({}).then(function(data){
+        console.log("-db.Test:data-")
+        console.log(data);
+
+        var hbObj = {
+            users: data
+        };
+
+        res.render("index", hbObj);
+    });
+});
 
 module.exports = router;
