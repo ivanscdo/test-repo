ebde21932f5a7c697d89f9181566bbf2c8b0a426
Ivan <ivan.scdo@gmail.com>
2018-05-12 23:55:11 -0500

    sa512.23:53- grab img, post to dom

--- public/test.js
+++ public/test.js
@@ -6,6 +6,10 @@ $(function() {
             username: $("#signup-username").val().trim(),
             password: $("#signup-password").val().trim()
         };
+
+        console.log("-newUser-");
+        console.log(newUser);
+        
     
         $.post("/api/test", newUser).then(function(data) {
             console.log("-$.post:data-");
@@ -14,21 +18,53 @@ $(function() {
         });
     });
 
-    const target = document.getElementById('target');
 
-    target.addEventListener('drop', (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-  
-      doSomethingWithFiles(e.dataTransfer.files);
+    function doSomethingWithFiles(fileList) {
+        // console.log("doSomethingWithFiles fired!");
+
+        let file = null;
+    
+        for (let i = 0; i < fileList.length; i++) {
+            if (fileList[i].type.match(/^image\//)) {
+                file = fileList[i];
+                break;
+            }
+        }
+        console.log("-file-");
+        console.log(file);
+    
+        if (file !== null) {
+            // const output = document.getElementById('output');    
+            // output.src = URL.createObjectURL(file);
+            console.log("-URL.createObjectURL(file)-");            
+            console.log(URL.createObjectURL(file));
+            $("#output").attr("src", URL.createObjectURL(file));  
+        }
+    // END OF: function doSomethingWithFiles(fileList) {
+    }
+
+    // const fileInput = document.getElementById('file-input');
+    // fileInput.addEventListener('change', (e) => doSomethingWithFiles(e.target.files));
+    $("#image-capture").change(function(event) {
+        // console.log("image-capture fired!");
+        
+        var fileList  = event.target.files;
+        // console.log(event.target.files);
+        console.log("-fileList-");
+        console.log(fileList);
+
+        // var fileList_Arr = Array.from(event.target.files);
+        // console.log(fileList_Arr);
+
+        doSomethingWithFiles(fileList);
+
+        // $("#show-picture").attr(src="");
     });
-  
-    target.addEventListener('dragover', (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-  
-      e.dataTransfer.dropEffect = 'copy';
-    });
 
 
+
+
+// END OF: $(function() {
 });
+
+
