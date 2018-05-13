187cc7b35065303320d85fc74157582ded3a5e70
Ivan Saucedo <ivan.scdo@gmail.com>
2018-05-12 20:28:05 -0500

    sa512.20:27- testing image capture

--- public/test.js
+++ public/test.js
@@ -13,4 +13,22 @@ $(function() {
             location.reload();
         });
     });
+
+    const target = document.getElementById('target');
+
+    target.addEventListener('drop', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+  
+      doSomethingWithFiles(e.dataTransfer.files);
+    });
+  
+    target.addEventListener('dragover', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+  
+      e.dataTransfer.dropEffect = 'copy';
+    });
+
+
 });
